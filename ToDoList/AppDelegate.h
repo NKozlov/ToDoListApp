28976529f74a271b79ce9db4483ab494f8d76df6
Kozlov Nikita <PravProduct@imac-kozlov.dlink>
2014-01-19 20:27:39 +0400

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Kozlov Nikita on 19.01.14.
+//  Copyright (c) 2014 Kozlov Nikita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
